d52613cb01c2f2ce913927db389e75792e129d3e
Davide <davide31122007@gmail.com>
2025-07-20 15:10:45 +0200

    Update

--- script.js
+++
@@ -1,9 +0,0 @@
-function lang_check() {
-	if (window.navigator.language != "it-IT")
-		window.location.href = "index.spa.html";
-	else
-		window.location.href = "index.ita.html";
-}
-
-if (window.location.href == "https://portauralis.github.io/" || window.location.href == "https://portauralis.github.io/index.html")
-	lang_check();
